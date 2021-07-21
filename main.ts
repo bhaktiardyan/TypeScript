bca917624f23d37bfe95d03a8be66e13e61e4cc3
Bhakti Ardyan <bhaktiardyan@gmail.com>
2021-07-21 19:13:50 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+//--- call object with class
 import { Kendaraan } from "./Kendaraan";
 
 let sedan =  new Kendaraan("sedan",4,"darat")
@@ -11,3 +12,24 @@ truk._Propertis()
 
 let pesawat = new Kendaraan("boing",150,"udara")
 pesawat._Propertis()
+
+
+//--- call object with class abstract
+import { mobil } from "./mobil";
+import { pesawat } from "./pesawat";
+
+let sedan = new mobil("sedan", 4, "Darat")
+sedan._Propertis()
+sedan.Maju()
+sedan.Mundur()
+sedan.Terbang()
+sedan._setValue("A")
+sedan._setValue(10)
+
+let boeing = new pesawat("boeing", 150, "udara")
+boeing._Propertis()
+boeing.Maju()
+boeing.Mundur()
+boeing.Terbang()
+boeing._setValue("A")
+boeing._setValue(10)
